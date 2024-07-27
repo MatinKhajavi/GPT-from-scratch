384edee46026c8b55fe26b8d2d6cc2950ad9cd06
MatinKhajavi <mat.khajavi@gmail.com>
2024-07-26 18:59:07 -0700

    feat: implement the generate function for GPT class.

--- src/models/gpt.py
+++ src/models/gpt.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from src.models._modules import *
 from typing import Dict, Any
 from dataclasses import dataclass
-
+from typing import Optional
 
 @dataclass
 class GPTConfig:
@@ -66,6 +66,53 @@ class GPT(nn.Module):
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, std=0.02)
     
+    @torch.no_grad()
+    def generate(self, idx: torch.Tensor, max_new_tokens: int, temperature: float = 1.0, top_k: Optional[int] = 50) -> torch.Tensor:
+        """
+        Generate new tokens based on the given input indices.
+
+        This function generates new tokens for the input sequence up to a specified
+        number of new tokens. The process involves adjusting the logits using the
+        provided temperature and optionally applying top-k sampling.
+
+        :param idx: A tensor of shape (batch_size, sequence_length) containing the input indices.
+        :type idx: torch.Tensor
+        :param max_new_tokens: The maximum number of new tokens to generate.
+        :type max_new_tokens: int
+        :param temperature: The temperature value for scaling the logits. Default is 1.0.
+        :type temperature: float
+        :param top_k: The number of top logits to consider for sampling. Default is 50.
+        :type top_k: Optional[int]
+        :return: A tensor containing the input indices concatenated with the generated new tokens.
+        :rtype: Tensor
+        """
+
+        self.eval()
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -self.cfg.context_length:]
+            
+            logits = self(idx_cond)
+            logits = logits[:, -1, :]
+
+            if top_k is not None:
+                top_logits, _ = torch.topk(logits, top_k)
+                min_val = top_logits[:, -1]
+                logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
+
+            assert temperature >= 0.0
+
+            if temperature > 0.0:
+                logits = logits / temperature
+                probs = torch.softmax(logits, dim=-1)
+                idx_next = torch.multinomial(probs, num_samples=1)  
+            else:
+                idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+            idx = torch.cat((idx, idx_next), dim=1)
+        
+        return idx
+            
+
 
     def forward(self, in_idx: torch.Tensor) -> torch.Tensor:
         """
