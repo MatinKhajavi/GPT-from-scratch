fd9e6780c727e3587d6608daa214609eb7a928cf
matinkhajavi <mat.khajavi@gmail.com>
2024-07-28 06:05:40 +0000

    update GPTConfig dropout rate.

--- src/models/utils.py
+++ src/models/utils.py
@@ -7,7 +7,7 @@ class GPTConfig:
     vocab_size: int = 50257  
     emb_dim: int = 768       
     context_length: int = 1024  
-    drop_rate: float = 0.1  
+    drop_rate: float = 0.1 
     n_layers: int = 12      
     n_heads: int = 12       
     qkv_bias: bool = False  
